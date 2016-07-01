908897249ffa7e11a882a1d21108f0e8a86edb20
Tim Pender <mrtimpender@gmail.com>
2016-07-01 13:59:22 -0600

    users list populating

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var data = require('../lib/data');
+
+var app = express();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/users', function(req, res, next) {
+  res.json(data);
+});
+
+
+module.exports = router;
---
+++ users.js
@@ -0,0 +1,31 @@
+var table = document.getElementById('userTable');
+
+httpRequest = new XMLHttpRequest();
+
+httpRequest.onreadystatechange = function(){
+    if (httpRequest.readyState === 4) {
+       if(httpRequest.status < 400) {
+        populateDOM(JSON.parse(httpRequest.responseText));
+        }
+    }
+
+};
+httpRequest.open('GET', 'http://localhost:3000/users');
+httpRequest.send();
+
+function populateDOM(array){
+  for (var i=0; i<array.length; i++){
+    var row = table.insertRow(i+1);
+    var cell0 = row.insertCell(0);
+    var cell1 = row.insertCell(1);
+    var cell2 = row.insertCell(2);
+    var cell3 = row.insertCell(3);
+    var cell4 = row.insertCell(4);
+
+    cell0.innerHTML = array[i].user_data.first_name;
+    cell1.innerHTML = array[i].user_data.last_name
+    cell2.innerHTML = array[i].user_data.id
+    cell3.innerHTML = array[i].user_data.active
+    cell4.innerHTML = array[i].user_data.y_code
+  }
+};
